b468663c848154acbbf1c2a0e22a3d31216ad2aa
Chidrewar Sanket <59963910+SanketChidrewar@users.noreply.github.com>
2024-03-31 15:02:39 +0000

    basic react app with cdn

---
+++ Sanket/Chapter 01 - Inception/Coding/App.js
@@ -0,0 +1,6 @@
+
+let heading = React.createElement("h1",{id: "head"}, "Hello World from React");
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+console.log(heading);
