3b6ea3d6c0f2c56bcc9becb650c8f73219bf5417
Chidrewar Sanket <59963910+SanketChidrewar@users.noreply.github.com>
2024-03-31 15:13:49 +0000

    creating html elements using react dom methods

--- Sanket/Chapter 01 - Inception/Coding/App.js
+++ Sanket/Chapter 01 - Inception/Coding/App.js
@@ -1,6 +1,29 @@
+// let heading = React.createElement("h1",{id: "head"}, "Hello World from React");
+// let root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
 
-let heading = React.createElement("h1",{id: "head"}, "Hello World from React");
-let root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// console.log(heading);
 
-console.log(heading);
+//create below dom with react
+{
+  /* <div id="parent">
+        <div id="child">
+            <h1></h1>
+        </div>
+    </div> 
+*/
+}
+
+let ele = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    [React.createElement("h1", { id: "parent" }, "inside h1 tag"),
+    React.createElement("h2", { id: "parent" }, "inside h2 tag")]
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(ele);
